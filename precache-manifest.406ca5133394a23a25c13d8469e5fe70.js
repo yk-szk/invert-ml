a8d3b9c5e58cb91292b54c5992318d7ac2fb564a
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-06-04 15:13:44 +0900

    Updates

---
+++ precache-manifest.406ca5133394a23a25c13d8469e5fe70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1e3971ea26461a62c4cbb082c03361",
+    "url": "/invert-ml/index.html"
+  },
+  {
+    "revision": "a444cda588ffd9e6c591",
+    "url": "/invert-ml/static/css/main.d5cf9673.chunk.css"
+  },
+  {
+    "revision": "8b39208e056292272f13",
+    "url": "/invert-ml/static/js/2.0eef85a2.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/invert-ml/static/js/2.0eef85a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a444cda588ffd9e6c591",
+    "url": "/invert-ml/static/js/main.1d89b5f1.chunk.js"
+  },
+  {
+    "revision": "7b3746c42ac5e0f90d9c",
+    "url": "/invert-ml/static/js/runtime-main.412d3a43.js"
+  }
+]);
